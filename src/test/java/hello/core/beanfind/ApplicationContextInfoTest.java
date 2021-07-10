30326325b25aac6b2f7529eb81002f3d27e4cbb7
jaeikey <leeji0922@gmail.com>
2021-07-10 21:55:32 +0900

    스프링 빈 등록 조회 기본

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -8,14 +8,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 class ApplicationContextInfoTest {
 
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class); //스프링 컨테이너 생성
 
     @Test
     @DisplayName("모든 빈 출력하기")
     void findAllBean(){
-        String[] BeanDefinitionNames = ac.getBeanDefinitionNames();
+        String[] BeanDefinitionNames = ac.getBeanDefinitionNames(); // 스프링에 등록된 모든 빈 이름을 조회
         for (String BeanDefinitionName : BeanDefinitionNames) {
-            Object bean = ac.getBean(BeanDefinitionName);
+            Object bean = ac.getBean(BeanDefinitionName); //BeanDefinitionName에 등록된 빈(ac 컨테이너 안의 빈)으로 객체를 조회한다
             System.out.println("name = " + BeanDefinitionName + " object = " + bean);
         }
     }
@@ -26,6 +26,8 @@ class ApplicationContextInfoTest {
         for (String BeanDefinitionName : BeanDefinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(BeanDefinitionName);
 
+            //Role ROLE.APPLICATION : 직접 등록한 애플리케이션 빈
+            //Role ROLE.INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
             if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
                 Object bean = ac.getBean(BeanDefinitionName);
                 System.out.println("name = " + BeanDefinitionName + " object = " + bean);
