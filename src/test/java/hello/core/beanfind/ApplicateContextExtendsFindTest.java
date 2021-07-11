d4db0f2064f462eaec25d46a5bcce82777535c2d
jaeikey <leeji0922@gmail.com>
2021-07-11 13:40:07 +0900

    xml방식으로 bean등록

--- src/test/java/hello/core/beanfind/ApplicateContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicateContextExtendsFindTest.java
@@ -30,7 +30,7 @@ public class ApplicateContextExtendsFindTest {
     @Test
     @DisplayName("부모 타입으로 조회시 자식이 둘 이상 있으면, 중복 오류가 발생한다.")
     void findBeanByParentTypeBeanName() {
-        DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy",DiscountPolicy.class);
+        DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
         assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
     }
 
@@ -66,6 +66,7 @@ public class ApplicateContextExtendsFindTest {
         public DiscountPolicy rateDiscountPolicy() {
             return new RateDiscountPolicy();
         }
+
         @Bean
         public DiscountPolicy fixDiscountPolicy() {
             return new FixDiscountPolicy();
